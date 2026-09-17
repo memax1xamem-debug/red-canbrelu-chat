b1fa2116bfcf579dd3b90aedbb500bbab4744845
memax1xamem-debug <memax1xamem@gmail.com>
2026-09-17 17:00:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -29,10 +29,11 @@ const opciones = {
   icon: "./icon-192.png",
   badge: "./icon-192.png",
   data: {
-    url: "./"
+    url: "./",
+    usuarioId: payload.data?.usuarioId || ""
   }
 };
-
+  
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
 
